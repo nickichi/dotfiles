734e28e021fa8944748bacdbbcf12d8b504c4bf1
市川貴邦 <nickichi@nickichi-book.local>
2017-08-01 19:30:46 +0900

    add history

--- zsh/zsh_common.zsh
+++ zsh/zsh_common.zsh
@@ -1,1 +1,13 @@
 PATH=$PATH:/usr/local/bin:/usr/bin
+
+# 履歴を保存する
+export HISTFILE=${HOME}/.zsh_history
+# メモリ履歴の件数
+export SAVESIZE=1000
+# ファイルに保存する履歴件数
+export SAVEHIST=100000
+# 重複を記録しない
+setopt hist_ignore_dups
+# 開始と終了を記録
+setopt EXTENDED_HISTORY
+
