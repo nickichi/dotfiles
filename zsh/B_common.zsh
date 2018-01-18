c896460460ac73cab85085868d3421f524cb420b
市川貴邦 <nick@nickbook.local>
2018-01-18 14:44:40 +0900

    history settings

--- zsh/B_common.zsh
+++ zsh/B_common.zsh
@@ -1,11 +1,14 @@
 PATH=$PATH:/usr/local/bin:/usr/bin
 
 # VIM設定
-export XDG_CONFIG_HOME=~/.dotfiles
+export XDG_CONFIG_HOME=${HOME}/.dotfiles
 # 履歴を保存する
 export HISTFILE=${HOME}/.zsh_history
+# 履歴サイズとフォーマット
+export HISTSIZE=100000
+export HISTTIMEFORMAT='%Y/%m/%d %H:%M:%S '
 # メモリ履歴の件数
-export SAVESIZE=1000
+export SAVESIZE=100000
 # ファイルに保存する履歴件数
 export SAVEHIST=100000
 # 重複を記録しない
